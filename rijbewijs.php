205b5c85289d0fbf82f0dda70cecfa96a1a350d6
Justin Plein <2099449@talnet.nl>
2020-01-14 14:27:31 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,8 @@
+<?php
+    $leeftijd = readline("Hoe oud ben je ?");
+    if($leeftijd <= "16,5") {
+        echo ("Helaas, je mag nog niet beginnen met rijlessen ");
+    }else {
+        echo "Je mag beginnen met rijlessen!";
+    }
+?>
